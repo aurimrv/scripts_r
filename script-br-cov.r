0d32543e4a4e65f30b0ae0ba6bc690fa78e99a50
Lucca <luccaguerino@estudante.ufscar.br>
2023-04-13 08:32:12 -0300

    Feat: Correction of compiled csv files and creation/execution of R programs

---
+++ script-br-cov.r
@@ -0,0 +1,150 @@
+# The collums corresponding test sets:
+#X1 = RANDOM
+#X2 = DYNAMOSA
+#X3 = MIO
+#X4 = MOSA
+#X5 = WHOLE_SUITE
+#X6 = DYNAMOSA-MIO
+#X7 = DYNAMOSA-MOSA
+#X8 = DYNAMOSA-WHOLE_SUITE
+#X9 = MIO-MOSA
+#X10 = MIO-WHOLE_SUITE
+#X11 = MOSA-WHOLE_SUITE
+#X12 = DYNAMOSA-MIO-MOSA
+#X13 = DYNAMOSA-MIO-WHOLE_SUITE
+#X14 = DYNAMOSA-MOSA-WHOLE_SUITE
+#X15 = MIO-MOSA-WHOLE_SUITE
+#X16 = DYNAMOSA-MIO-MOSA-WHOLE_SUITE
+
+#Lendo dados
+cov<-read.csv("compiled-branch-coverage-report.csv",sep=";",dec=".",header=TRUE)
+cov
+summary(cov)
+
+setEPS()
+postscript("branch-cov-boxplot.eps")
+png('branch-cov-boxplot.png')
+boxplot(cov,ylab="Branch Coverage",xlab="Test Sets",names=c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"))
+
+dev.off()
+
+#Teste de Homogeniedade
+# p < 0.05 rejects the null hypothesis and conclude that not all groups have the same variance
+bartlett.test(cov)
+
+#Teste de Normalidade
+# p < 0.05 rejects the null hypothesis that the data are from a normally distributed population
+shapiro.test(cov$X1)
+shapiro.test(cov$X2)
+shapiro.test(cov$X3)
+shapiro.test(cov$X4)
+shapiro.test(cov$X5)
+shapiro.test(cov$X6)
+shapiro.test(cov$X7)
+shapiro.test(cov$X8)
+shapiro.test(cov$X9)
+shapiro.test(cov$X10)
+shapiro.test(cov$X11)
+shapiro.test(cov$X12)
+shapiro.test(cov$X13)
+shapiro.test(cov$X14)
+shapiro.test(cov$X15)
+shapiro.test(cov$X16)
+
+attach(cov)
+
+#Colocando dados na vertical
+cov.vert<-data.frame(Coverage<-gl(16,21), TestSets<-c(X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12,X13,X14,X15,X16))
+cov.vert
+
+#Media
+tapply(TestSets, Coverage, mean)
+
+#Desvio Padrão
+tapply(TestSets, Coverage, sd)
+
+#Variânica
+tapply(TestSets, Coverage, var)
+
+kruskal.test(TestSets~Coverage, cov.vert)
+kruskal.test(list(X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12,X13,X14,X15,X16))
+
+# Wilcox Test
+print("Wilcox Test X1 e X2")
+x1x2 = wilcox.test(TestSets[1:21],TestSets[22:42],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X3")
+x1x3 = wilcox.test(TestSets[1:21],TestSets[43:63],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X4")
+x1x4 = wilcox.test(TestSets[1:21],TestSets[64:84],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X5")
+x1x5 = wilcox.test(TestSets[1:21],TestSets[85:105],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X6")
+x1x6 = wilcox.test(TestSets[1:21],TestSets[106:126],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X7")
+x1x7 = wilcox.test(TestSets[1:21],TestSets[127:147],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X8")
+x1x8 = wilcox.test(TestSets[1:21],TestSets[148:168],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X9")
+x1x9 = wilcox.test(TestSets[1:21],TestSets[169:189],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X10")
+x1x10 = wilcox.test(TestSets[1:21],TestSets[190:210],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X11")
+x1x11 = wilcox.test(TestSets[1:21],TestSets[211:231],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X12")
+x1x12 = wilcox.test(TestSets[1:21],TestSets[232:252],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X13")
+x1x13 = wilcox.test(TestSets[1:21],TestSets[253:273],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X14")
+x1x14 = wilcox.test(TestSets[1:21],TestSets[274:294],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X15")
+x1x15 = wilcox.test(TestSets[1:21],TestSets[295:315],paired=TRUE,alternative="two.sided")
+
+# Wilcox Test
+print("Wilcox Test X1 e X16")
+x1x16 = wilcox.test(TestSets[1:21],TestSets[316:336],paired=TRUE,alternative="two.sided")
+
+
+p=c(x1x2$p.value, x1x3$p.value, x1x4$p.value, x1x5$p.value, x1x6$p.value, 
+    x1x7$p.value, x1x8$p.value, x1x9$p.value, x1x10$p.value, x1x11$p.value, 
+    x1x12$p.value, x1x13$p.value, x1x14$p.value, x1x15$p.value, x1x16$p.value)
+p
+
+# https://stat.ethz.ch/R-manual/R-devel/library/stats/html/p.adjust.html
+padj=p.adjust(p, method = "holm", n = length(p))
+#padj=p.adjust(p, method = "bonferroni", n = length(p))
+padj
+padj[1]
+cat("Test Sets;", "p-value;", "adjusted p-value;", "\nx1x2;", x1x2$p.value, ";", padj[1], 
+    "\nx1x3;", x1x3$p.value, ";", padj[2], "\nx1x4;", x1x4$p.value, ";", padj[3], 
+    "\nx1x5;", x1x5$p.value, ";", padj[4], "\nx1x6;", x1x6$p.value, ";", padj[5], 
+    "\nx1x7;", x1x7$p.value, ";", padj[6], "\nx1x8;", x1x8$p.value, ";", padj[7], 
+    "\nx1x9;", x1x9$p.value, ";", padj[8], "\nx1x10;", x1x10$p.value, ";", padj[9], 
+    "\nx1x11;", x1x11$p.value, ";", padj[10], "\nx1x12;", x1x12$p.value, ";", padj[11], 
+    "\nx1x13;", x1x13$p.value, ";", padj[12], "\nx1x14;", x1x14$p.value, ";", padj[13], 
+    "\nx1x15;", x1x15$p.value, ";", padj[14], "\nx1x16;", x1x16$p.value, ";", padj[15])
